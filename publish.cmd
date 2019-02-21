a32bd8c051702f3f0d5517cf1d781487ea7d8547
ranjitsinghsodhi <45357950+ranjitsinghsodhi@users.noreply.github.com>
2019-02-21 10:24:17 +0000

    Initial commit

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=csdbot2-Web-Deploy.pubxml -p:Password=9nA2oQLgoSjxAgquTArAn13p1FtYYNHcgzPBfR2Q3dkryb62DrBsd6kr5Gcy
+
